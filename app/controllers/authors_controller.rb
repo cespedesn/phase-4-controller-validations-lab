c93ac9209341a07dcbf0cb704dca0fd7218d5619
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-11-03 13:43:48 -0400

    commit tree 564f6d6eaf7a39d25310533532b8c73877eb6fcf

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,16 +1,20 @@
 class AuthorsController < ApplicationController
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_author_not_found_response
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_post_not_found_response
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
-    render json: author, status: :created
+    author = Author.create!(author_params)
+      render json: author, status: :created
+  rescue ActiveRecord::RecordInvalid => invalid
+      render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
+  
 
   private
   
@@ -18,4 +22,17 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+  # def render_author_not_found_response
+  #   render json: { error: "Author not found" }, status: :not_found
+  # end
+
+  # def render_post_not_found_response
+  #   render json: { error: "Post not found" }, status: :not_found
+  # end
+
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  # end
+
+
 end
